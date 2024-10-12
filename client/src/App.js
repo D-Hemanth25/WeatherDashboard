9b540ca971b050d6b7f7bb54b9aac52722102c9a
D-Hemanth25 <mailhemanth25@gmail.com>
2024-10-12 16:40:11 +0530

    Create starter react client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./Dashboard";
+
+function App(){
+    return(
+        <>
+            <h1>Weather Dashboard</h1>
+            <Dashboard />
+        </>
+    );  
+};
+
+export default App;
---
+++ client/src/Dashboard.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function Dashboard(){
+    const [items, setItems] = useState(["marlboro", "camel"]); 
+    const [item, setItem] = useState("");
+
+    const addItem = ()=> {
+        setItems([...items, item]);
+        setItem("");
+    };
+
+    const deleteItem = (name)=> {
+        setItems(items.filter((item) => item !== name))
+    };
+
+    return(
+        <>
+            <input text="text" placeholder="enter location" onChange={ (event)=> setItem(event.target.value)} />
+            <button onClick={addItem}>Add</button>
+            <ul>
+                {items.map((name) => (
+                    <div>
+                        <li key={name}>{name}</li>
+                        <button onClick={() => deleteItem(name)}>Delete</button>
+                    </div>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default Dashboard;
